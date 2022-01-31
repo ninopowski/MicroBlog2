84f3bfc05fe59e7897ab859b07662f385f3c5473
ninopowski <ninoskbd@gmail.com>
2022-01-31 11:11:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import datetime
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://ninopowski:nnikojdrug@newcluster.xdhrr.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE")
+app.db = client.db1
+
+
+entries = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    print([e for e in app.db.MicroBlog.find({})])
+    posts = entries
+    if request.method == "POST":
+        title = request.form["title"]
+        text = request.form["field"]
+        date = datetime.datetime.now().strftime("%b %Y")
+        new_entry = {
+            "title": title,
+            "text": text,
+            "date": date
+        }
+        entries.append(new_entry)
+        return redirect(url_for('home'))
+    return render_template("index.html", posts=posts)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
